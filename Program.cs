25679934071f4b7769b4220406b036d3933e25d0
KirByxYoung <istegomen@ya.ru>
2022-07-22 22:14:02 +0400

    Did task8

--- Program.cs
+++ Program.cs
@@ -1,12 +1,3 @@
-﻿class Weapon
-{
-    private int _bullets;
-
-    public void Shoot()
-    {
-        if (CanShoot())
-            _bullets--;
-    }
-
-    private bool CanShoot() => _bullets > 0;
-}
+﻿int armySize = 10;
+int coins = 10;
+string name = "Vladislav";
